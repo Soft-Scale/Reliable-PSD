4489aaaa0cb4105071314b46ba914e9d72dd0606
Stanislav Orekhovskiy <stanislav.orekhovskiy@gmail.com>
2021-06-30 09:46:15 +0300

    Layout test task

---
+++ app/assets/scripts/index.js
@@ -0,0 +1,35 @@
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("header");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("header-sticky");
+  } else {
+    header.classList.remove("header-sticky");
+  }
+}
+
+/* Burger mobile menu */
+document.querySelector(".menu").addEventListener('click', function(){
+  console.log(document.querySelector(".nav"));
+  document.querySelector(".nav").classList.toggle("active")
+});
